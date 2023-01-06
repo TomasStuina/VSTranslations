de00fb1f247beb3ad30089cc58f0738792c6f716
Tomas <tomas.stuina@gmail.com>
2023-01-06 22:47:56 +0200

    Add initial solution with VSIX package project

---
+++ tests/VsTranslations.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VsTranslations.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
