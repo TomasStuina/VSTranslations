fe7c1a557ab9864d841d768133bdfb74dad923d9
Tomas <tomas.stuina@gmail.com>
2023-01-16 18:33:49 +0200

    Add ObjectExtensionsTests

--- tests/VSTranslations.Common.UnitTests/Extensions/ObjectExtensionsTests.cs
+++ tests/VSTranslations.Common.UnitTests/Extensions/ObjectExtensionsTests.cs
@@ -1,6 +1,27 @@
-﻿namespace VSTranslations.Common.UnitTests.Extensions
+﻿using FluentAssertions;
+using VsTranslations.UnitTests.AutoFixture.Attributes;
+using VSTranslations.Common.Extensions;
+
+namespace VSTranslations.Common.UnitTests.Extensions
 {
-    internal class ObjectExtensionsTests
+    public class ObjectExtensionsTests
     {
+        [Theory]
+        [DefaultAutoData]
+        public void ThrowIfNull_WhenInstanceIsNull_ShouldThrowArgumentNullException(string paramName)
+        {
+            // Act & Assert
+            FluentActions.Invoking(() => ObjectExtensions.ThrowIfNull<string?>(null, paramName))
+                .Should().Throw<ArgumentNullException>()
+                .Which.ParamName.Should().Be(paramName);
+        }
+
+        [Theory]
+        [DefaultAutoData]
+        public void ThrowIfNull_WhenInstanceIsNotNull_ShouldReturnSameInstance(string instance, string paramName)
+        {
+            // Act & Assert
+            instance.ThrowIfNull(paramName).Should().BeSameAs(instance);
+        }
     }
 }
